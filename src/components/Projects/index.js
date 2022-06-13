321c0df7d2a957a2aa8d63c3adc2df116c83bd52
Filipe Leoni <Filip@LAPTOP-A1HR45F9>
2022-06-13 14:54:01 -0300

    Add new archives

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -12,9 +12,9 @@ function Projects() {
         <h2 id="title">My Projects</h2>
         <div className='projects-comp'>
         <Component name="Calculator React" description="The project consists of a simple calculator made with ReactJS" link="https://react-calculator-seven-gold.vercel.app/" img={Calculator} github="https://github.com/FilipeLeoni/React-Calculator-"/>
-        <Component name="My Website" description="I designed and implemented the site you are browsing right now!" link="https://react-calculator-seven-gold.vercel.app/" img={Portfolio} github="https://github.com/FilipeLeoni/React-Calculator-"/>
+        <Component name="My Website" description="I designed and implemented the site you are browsing right now!" link="https://github.com/FilipeLeoni/My-Website" img={Portfolio} github="https://github.com/FilipeLeoni/My-Website"/>
         <Component name="Landing Page " description="A responsive website made with html, css and js. The Project is under development." link="https://focused-leakey-3e4c4c.netlify.app/" img={Landing} github="https://github.com/FilipeLeoni/Responsive-Website"/>
-        <Component name="Cep Finder" description="Website with the functionality to find the address by the typed zip code."link="https://react-calculator-seven-gold.vercel.app/" img={Buscador} github="https://github.com/FilipeLeoni/React-Calculator-"/>
+        <Component name="Cep Finder" description="Website with the functionality to find the address by the typed zip code."link="https://github.com/FilipeLeoni/Buscar-cep-ReactJS" img={Buscador} github="https://github.com/FilipeLeoni/Buscar-cep-ReactJS"/>
         </div>
     </section>
   )
